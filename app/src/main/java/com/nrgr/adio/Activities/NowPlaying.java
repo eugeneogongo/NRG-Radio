fa5f6a46d203bc95c396c29aa911aea7edf652d9
Neptune <eugeneogongo@live.com>
2019-04-30 11:59:59 +0300

    Now Playing WIndow

---
+++ app/src/main/java/com/nrgr/adio/Activities/NowPlaying.java
@@ -0,0 +1,16 @@
+package com.nrgr.adio.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.nrgr.adio.R;
+
+public class NowPlaying extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_now_playing);
+    }
+}
